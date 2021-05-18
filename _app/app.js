dc26b7ea27b89ab3e3e6be34a6ac08ce7a3e3815
Usman Office <stores.yarncraft@gmail.com>
2021-05-18 16:58:21 +0500

    Make task list skeleton

--- _app/app.js
+++ _app/app.js
@@ -1,19 +1,28 @@
 // INITAILIZE VARIABLES
 
-const mainBody = document.querySelector('body');
-const themeToggle = document.querySelector('#theme-toggle');
-const formDiv = document.querySelector('#form-div');
-const taskInput = document.querySelector('#task-input');
+const mainBody = document.querySelector("body");
+const themeToggle = document.querySelector("#theme-toggle");
+const formDiv = document.querySelector("#form-div");
+const taskInput = document.querySelector("#task-input");
+const mainSection = document.querySelector("#main-section");
+const innerCircle = document.querySelectorAll(".inner-circle");
+
+themeToggle.addEventListener("click", function () {
+  mainBody.classList.toggle("dark-body");
+  mainBody.classList.toggle("light-body");
+  themeToggle.classList.toggle("light-SVG");
+  themeToggle.classList.toggle("dark-SVG");
+  formDiv.classList.toggle("dark");
+  formDiv.classList.toggle("light");
+  taskInput.classList.toggle("light-input");
+  mainSection.classList.toggle("dark");
+  mainSection.classList.toggle("light");
+  mainSection.classList.toggle("section-dark");
+  mainSection.classList.toggle("section-light");
 
 
-themeToggle.addEventListener('click', function(){
-    mainBody.classList.toggle('dark-body');
-    mainBody.classList.toggle('light-body');
-    themeToggle.classList.toggle('light-SVG');
-    themeToggle.classList.toggle('dark-SVG');
-    formDiv.classList.toggle('dark');
-    formDiv.classList.toggle('light');
-    taskInput.classList.toggle('light-input');
-    document.querySelector('.inner-circle').classList.toggle('dark-circle');
-    document.querySelector('.inner-circle').classList.toggle('light-circle');
-})
+  innerCircle.forEach(function (circle) {
+    circle.classList.toggle("dark-circle");
+    circle.classList.toggle("light-circle");
+  });
+});
